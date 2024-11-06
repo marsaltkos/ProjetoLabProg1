5fd2fe9d9b1a39c02fe3b47c7691f36bbcb0b961
andrerosa98 <202420120000024@pucgo.edu.br>
2024-11-06 18:39:40 -0300

    melhorando a calculadora (exibição de números e opção de trocar os números)

--- math.java
+++ math.java
@@ -14,9 +14,9 @@ public class math{
         int opcao;
         
         System.out.print("Digite o primeiro número: ");
-        a = input.nextInt();
+        a = input.nextFloat();
         System.out.print("Digite o segundo número: ");
-        b = input.nextInt();
+        b = input.nextFloat();
 
         System.out.println("---------- MENU DE OPÇÕES ----------");
         System.out.println("1 - Somar");
@@ -24,6 +24,7 @@ public class math{
         System.out.println("3 - Multiplicar");
         System.out.println("4 - Dividir");
         System.out.println("5 - Encontrar o resto da divisão");
+        System.out.println("6 - Escolher outros dois números");
         System.out.println("0 - Sair");
         opcao = input.nextInt();
         input.nextLine();
@@ -33,27 +34,44 @@ public class math{
             switch (opcao){
                 case 1:
                     soma = funcoes.soma(a,b);
+                    System.out.println("---------- RESULTADO ---------------");
                     System.out.println(a + " + " + b + " = " + soma);
+                    System.out.println("------------------------------------");
                 break;
 
                 case 2:
                     subtracao = funcoes.subtracao(a,b);
+                    System.out.println("---------- RESULTADO ---------------");
                     System.out.println(a + " - " + b + " = " + subtracao);
+                    System.out.println("------------------------------------");
                 break;
 
                 case 3:
                     multiplicacao = funcoes.multiplicacao(a,b);
+                    System.out.println("---------- RESULTADO ---------------");
                     System.out.println(a + " * " + b + " = " + multiplicacao);
+                    System.out.println("------------------------------------");
                 break;
 
                 case 4:
                     divisao = funcoes.divisao(a,b);
+                    System.out.println("---------- RESULTADO ---------------");
                     System.out.println(a + " / " + b + " = " + divisao);
+                    System.out.println("------------------------------------");
                 break;
 
                 case 5:
                     resto = funcoes.resto(a,b);
+                    System.out.println("---------- RESULTADO ---------------");
                     System.out.println(a + " % " + b + " = " + resto);
+                    System.out.println("------------------------------------");
+                break;
+
+                case 6:
+                    System.out.print("Digite o primeiro número: ");
+                    a = input.nextFloat();
+                    System.out.print("Digite o segundo número: ");
+                    b = input.nextFloat();
                 break;
 
                 default:
@@ -62,12 +80,17 @@ public class math{
                     input.nextLine();
             }
 
-            System.out.println("---------- RESULTADO ----------");
+            System.out.println("\n");
+            System.out.println("Seus números são:");
+            System.out.println("número A " + a);
+            System.out.println("número B " + b);
+            System.out.println("---------- MENU DE OPÇÕES ----------");
             System.out.println("1 - Somar");
             System.out.println("2 - Subtrair");
             System.out.println("3 - Multiplicar");
             System.out.println("4 - Dividir");
             System.out.println("5 - Encontrar o resto da divisão");
+            System.out.println("6 - Escolher outros dois números");
             System.out.println("0 - Sair");
             opcao = input.nextInt();
             input.nextLine();  
