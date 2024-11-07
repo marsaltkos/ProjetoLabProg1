de47a471808ace01882b732cafb4811a34d2d549
andrerosa98 <202420120000024@pucgo.edu.br>
2024-11-06 21:46:35 -0300

    Início do projeto integrador de Laboratório de Programação

---
+++ FuncProjeto.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+
+public class FuncProjeto {
+    
+    Scanner input = new Scanner(System.in);
+
+    public static void Cabecalho(){
+    System.out.println("***** BEM VINDO *****");
+    System.out.println("1 - Cadastro");
+    System.out.println("2 - Login");
+    System.out.println("3 - Sobre nós");
+    System.out.println("4 - SAIR");
+    System.out.println("*********************");      
+    }
+}
---
+++ Projeto.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class Projeto{
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        
+        int opcao;
+        String senha;
+        String senhaRep = " ";
+        String usuario;
+
+        FuncProjeto.Cabecalho();
+        opcao = input.nextInt();
+
+        while (opcao != 4){
+        
+            switch(opcao){
+
+                case 1:
+                    System.out.print("Digite seu usuário: ");
+                    usuario = input.next();
+                    System.out.print("Digite sua senha: ");
+                    senha = input.next();
+                    System.out.print("Digite novamente sua senha: ");
+                    senhaRep = input.next();
+                
+                    if (senha.equals(senhaRep)){
+                        System.out.println("Usuário cadastrado com sucesso!");
+                        FuncProjeto.Cabecalho();
+                        opcao = input.nextInt();
+                    }else{
+                        while(!senhaRep.equals(senha)){
+                        System.out.println("Senha incorreta. Tente novamente: ");
+                        senhaRep = input.next();
+                        }
+                        System.out.println("Usuário cadastrado com sucesso!");  
+                        FuncProjeto.Cabecalho();
+                        opcao = input.nextInt(); 
+                    }
+                break;
+
+                case 2:
+                    System.out.print("Digite seu usuário: ");
+                    usuario = input.next();
+                    System.out.print("Digite sua senha: ");
+                    senha = input.next();
+
+                    if (senha.equals(senhaRep)){
+                        System.out.println("Usuário validado com sucesso! Seja bem vindo.");
+                    }else{
+                        while(!senha.equals(senhaRep)){
+                            System.out.print("Senha inválida, tente novamente: ");
+                            senha = input.next();
+                        }
+                    }
+                    FuncProjeto.Cabecalho();
+                    opcao = input.nextInt();
+                break;    
+    
+            }        
+        }
+    }
+}
