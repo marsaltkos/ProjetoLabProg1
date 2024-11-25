22b668704625eba476ef652989ec287ef5412c6f
andrerosa98 <202420120000024@pucgo.edu.br>
2024-11-24 23:12:38 -0300

    edição da sessão Sobre Nós

--- FuncProjeto.java
+++ FuncProjeto.java
@@ -14,4 +14,15 @@ public class FuncProjeto {
     System.out.println("4 - SAIR");
     System.out.println("*********************");      
     }
+
+    public static void SobreNos(){
+        System.out.println("+----------------------------------------+");
+        System.out.println("|         ***** TEAM MAIA *****          |");
+        System.out.println("| - André Luis Ferreira da Rosa Junior   |");
+        System.out.println("| - Carlos Eduardo Magalhães Silva       |");
+        System.out.println("| - Igor Cesar Caliman                   |");
+        System.out.println("| - Marsal Brito Santos                  |");
+        System.out.println("| - Rodrigo Pinheiro de Queiroz          |");
+        System.out.println("+----------------------------------------+");
+    }
 }
